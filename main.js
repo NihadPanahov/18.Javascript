ab988c5c2d657a0f1a11fcbac94065d8e82f8ac3
NihadPanahov <nihadpanah03@gmail.com>
2022-08-27 15:29:05 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// const dishes = ['cup', 'plate', 'spoon', 'knife','fork'];
+// let i = 0;
+// let text = "";
+
+// for (;dishes[i];) {
+//   text += dishes[i];
+//   i++;
+// }
+// console.log(dishes)
+
+
+
+// function numberSum(N) {
+//     var total = 0;
+//       for(var i = 1; i <= 100; i++){
+//         total += i;
+//       }
+//       return total;
+//   }
